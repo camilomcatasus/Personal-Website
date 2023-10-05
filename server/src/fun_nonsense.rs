db4bac9a4d86183c8730d92b569fa53c2e19655b
Camilo Catasus <camilomcatasus@gmail.com>
2023-10-05 00:09:23 -0400

    Created about page

--- server/src/fun_nonsense.rs
+++ server/src/fun_nonsense.rs
@@ -1,1 +1,10 @@
+use actix_web::{get, web, HttpResponse, HttpRequest};
+use models::AppState;
+use minijinja::context;
 pub mod htmx_snake;
+
+
+#[get("/fun-nonsense/about")]
+pub async fn fun_nonsense_about(app_state: web::Data<AppState>, req: HttpRequest) -> HttpResponse {
+    return app_state.render_template("fun_nonsense_about.html", &req, context! {  });
+}
