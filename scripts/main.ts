49d070ee4f385359aa24d5549a0c934d4c9c2176
Camilo Catasus <camilomcatasus@gmail.com>
2024-02-13 21:14:54 -0500

    Minor fixes

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,27 @@
 import { snakeKeyDownHandler } from "./htmx_snake";
 
+
+function toggleHidden(event: Event) {
+    let target = event.target as HTMLElement;
+    if (target.classList.contains("hidden")) 
+    {
+        target.classList.remove("hidden");
+        target.classList.add("flex");
+    }
+    else 
+    {
+        target.classList.add("hidden");
+        target.classList.remove("flex");
+    }
+}
+
 window.onload = () => {
     document.onkeydown = (event: KeyboardEvent) => {
         snakeKeyDownHandler(event);
+    }
+
+    document.onclick = (event: Event) => {
+        let target = event.target as HTMLElement;
+        if(target.matches("#toggle-nav")) toggleHidden(event);
     }
 }
